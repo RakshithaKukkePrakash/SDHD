89f0cad6341c8fa82608febf6aee469e0a9e8985
Rakshitha Kukke Prakash <rakshithakp1998@gmail.com>
2024-06-02 20:42:31 +0200

    Ex123
    Exercises complete

--- TxtoMATLAB/Ex2Uint8toMatlab/Ex2.cydsn/main.c
+++ TxtoMATLAB/Ex2Uint8toMatlab/Ex2.cydsn/main.c
@@ -11,28 +11,73 @@
 */
 #include "project.h"
 #include "stdlib.h"
-#define TRIANGLE_WAVE 0
+//#define TRIANGLE_WAVE 0
+//#define SQUARE_WAVE 0
+//#define SAWTOOTH_WAVE 0
+#define STEP_WAVE 0
+
 
 int main(void)
 {
     CyGlobalIntEnable; /* Enable global interrupts. */
     UART_Start();
-    uint16_t waveArray[256] = {0};
     UART_PutString("Exercise 2");
+    uint16_t wave[256] = {0};  
     
     #ifdef TRIANGLE_WAVE
-        for(uint16_t rise =0; rise < 129;rise++)
-        {
-            waveArray[rise]=rise;            
-        }
-        for(uint16_t fall =129; fall <255;fall++)
-        {
-            waveArray[fall]=255-fall;
-        }        
-        
+    for(uint16_t rise =0; rise <= 128;rise++)
+    {
+        wave[rise]=rise;            
+    }
+    for(uint16_t fall =129; fall <256;fall++)
+    {
+        wave[fall]=255-fall;
+    } 
+    for(uint16_t i = 0; i < 256; i++)
+    {
+        wave[i] = (i <= 128) ? i : 256 - i;
+    }
     #endif
     
+    #ifdef SQUARE_WAVE
+    // 50% dutycycle 
     
+    for(uint16_t i = 0; i < 256; i++)
+    {
+        wave[i] = (i < 128) ? 0 : 1;
+    }
+    
+    // Different duty cycles 
+    
+    float dutyCycle = 0.2; 
+
+    uint16_t highPeriod = 256 * dutyCycle; 
+    uint16_t lowPeriod = (256 - highPeriod) / 2; 
+
+    for(uint16_t i = 0; i < 256; i++)
+    {
+        wave[i] = (i < lowPeriod || i >= (256 - lowPeriod))? 0:1; 
+
+    }
+    #endif
+      
+    
+    #ifdef SAWTOOTH_WAVE
+    for(uint16_t i = 0; i < 256; i++)
+    {
+        wave[i] = (i <255) ? i : 0;
+    }
+    #endif
+    
+    #ifdef STEP_WAVE
+        uint16_t stepDuration = 32; 
+        for(uint16_t i = 0; i < 256; i++)
+        {
+            uint8_t level = i / stepDuration;
+            wave[i] = level;
+        }
+    #endif
+
     
     
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
@@ -41,12 +86,9 @@ int main(void)
     {
         if(UART_GetChar() == 's')
         {
-           for(uint16_t wave =0; wave < 256;wave++)
+           for(uint16_t i =0; i < 256;i++)
            {
-                //UART_PutChar(waveArray[wave]);
-                char log[20];
-                itoa(waveArray[wave], log, 10);
-                UART_PutString(log);
+                UART_PutChar(wave[i]);
            }
         }
     }
